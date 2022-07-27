99db2f8b34319a830f4bfcfe1ecc794a4cc6b1d7
Oliver Barnum <oliverbarnum32@gmail.com>
2022-07-27 08:36:46 -0700

    Use utf8 reading explicityly

--- intemplator/lib/templateProcessor/fileReader.py
+++ intemplator/lib/templateProcessor/fileReader.py
@@ -4,6 +4,6 @@ def GetTemplateFilePath(templateName):
     return os.path.join(os.path.dirname(__file__),'templates/' + templateName)
 
 def ReadJson (path):
-    with open(path, 'r') as jsonFile:
+    with open(path, "r", encoding="utf-8") as jsonFile:
         data = json.load(jsonFile)
         return data
